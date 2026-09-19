f59c10412dd961e843a5d3b2a04e82c8e8ee892d
Prudhvi tharak <104729346+Prudhvitharak@users.noreply.github.com>
2026-09-19 14:03:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function openBook(){
-
-document.body.style.transition="1s";
-document.body.style.opacity="0";
-
-setTimeout(() => {
-
-window.location.href="map.html";
-
-},1000);
-
+function openBook() {
+    alert("Mischief Managed");
 }
